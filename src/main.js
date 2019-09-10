0bd57ccd0f8245b926d06ba5ee26e1e20ef429f8
Erin Deuel and Loewy Malkovich <loewy@>
2019-09-10 11:53:52 -0700

    add API call for bulbasaur gif, in progress

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
   } else if (bulba.tooHungryWeOut() === false) {
     $('#feed').click(function(event) {
       event.preventDefault();
+
       let btn = $(this);
       btn.prop('disabled', true);
       setTimeout(function() {
@@ -19,6 +20,24 @@ $(document).ready(function() {
       }, 1000);
       bulba.feed();
       console.log('hunger after bulba fed', bulba.hunger);
+
+      let request = new XMLHttpRequest();
+      const url = 'https://api.giphy.com/v1/gifs/search?api_key=3PLINuqhz03BffpRF1GEqFiAsHS4Aqv0&q=bulbasaur&limit=5&offset=0&rating=PG&lang=en';
+
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      };
+
+      request.open("GET", url, true);
+      request.send();
+
+      const getElements = function(response) {
+        $('#bulba-been-fed').text(`${response.data[0].images.original.mp4}`);
+      };
+
     });
   }
 });
